c95958392ad05cdf920fd81c78cc0337729a38db
Tahir <tahir.kutty@gmail.com>
2025-08-13 23:37:27 +0530

    3 new projects added which will showcase gsap and tailwind skills

--- src/utils/PortfolioData.js
+++ src/utils/PortfolioData.js
@@ -1,21 +1,39 @@
 import {
+  velvetPourPreview,
+  edgeAiPreview,
+  redefineGamingPreview,
   comfyPreview,
-  comfyPreviewWebp,
   weddingPreview,
-  weddingPreviewWebp,
   colorGeneratorPreview,
-  colorGeneratorPreviewWebp,
   stageCoachPreview,
-  stageCoachPreviewWebp,
   foodOrderPreview,
-  foodOrderPreviewWebp
 } from "../constants";
 
 export const work = [
   {
+    redirect: "https://tahir29.github.io/velvetPour/",
+    preview: velvetPourPreview,
+    title: "Velvet Pour",
+    description:
+      "Velvet Pour is a fresh, visually pleasing showcase built with Vite, GSAP, and Tailwind CSS. It features a smooth scroll-triggered video animation and a custom slider that fluidly highlights content. With Tailwind powering the clean layout and GSAP delivering polished transitions, the site offers an immersive, modern experience.",
+  },
+  {
+    redirect: "https://tahir29.github.io/edgeAI/",
+    preview: edgeAiPreview,
+    title: "Edge AI",
+    description:
+      "Edge AI is a sleek, responsive site built with Vite and Tailwind CSS, featuring a smooth light/dark theme toggle. It combines clean design, fast performance, and user-friendly interaction to showcase modern front-end development skills.",
+  },
+  {
+    redirect: "https://tahir29.github.io/redefineGaming/",
+    preview: redefineGamingPreview,
+    title: "Redefine Gaming",
+    description:
+      "Redefine Gaming is a sleek, interactive gaming showcase built with modern web tools like Vite, GSAP, and Tailwind CSS. This site features eye-catching animations, seamless transitions, and responsive design—all crafted to immerse visitors in a dynamic, polished experience. Tailwind CSS ensures a clean and efficient layout, while GSAP brings fluid, attention-grabbing effects that elevate every interaction.",
+  },
+  {
     // redirect: "https://comfy-sloth-new.netlify.app/",
     redirect: "https://tahir29.github.io/comfySloth/",
-    webp: comfyPreviewWebp,
     preview: comfyPreview,
     title: "Comfy Sloth",
     description:
@@ -23,15 +41,13 @@ export const work = [
   },
   {
     redirect: "https://tahir29.github.io/ReactWedding/",
-    webp: weddingPreviewWebp,
     preview: weddingPreview,
     title: "Wedding",
     description:
-      "Welcome to the official wedding website. Here, you'll find everything you need to know about the upcoming celebration of love. From the heartfelt ceremony and lively reception details to travel suggestions, It's all here. Join us as we unite our lives and create cherished memories together. We can't wait to share this special day with you!",
+      "Welcome to the official wedding website. Here, you'll find everything you need to know about the upcoming celebration of love. From the heartfelt ceremony and lively reception details to travel suggestions, It's all here.",
   },
   {
     redirect: "https://tahir29.github.io/color-generator/",
-    webp: colorGeneratorPreviewWebp,
     preview: colorGeneratorPreview,
     title: "Color Generator",
     description:
@@ -39,7 +55,6 @@ export const work = [
   },
   {
     redirect: "https://www.montereystagecoachlodge.com/",
-    webp: stageCoachPreviewWebp,
     preview: stageCoachPreview,
     title: "Monterey Stage Coach Lodge",
     description:
@@ -47,7 +62,6 @@ export const work = [
   },
   {
     redirect: "https://tahir29.github.io/Food-Order/",
-    webp: foodOrderPreviewWebp,
     preview: foodOrderPreview,
     title: "Food Order",
     description:
